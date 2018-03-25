03ceaa29941d11da8715df2e03117fe845e9d036
khemeioa <tasha.s.chin@gmail.com>
2018-03-25 13:20:27 -0700

    Initial thoughts on approaching decoder challenge.

---
+++ decode.py
@@ -0,0 +1,8 @@
+def decode(s):
+    """Decode a string."""
+
+    # may need to split assertion. valid inputs can have numbers after 0-index.
+    assert s[0].isdigit() and s[1:].isalpha(), "String must contain a sequence that starts with a number and end with letter(s)."
+
+    # will need to store ints as slice starts. grab by index?
+    # at end, need to concatenate all valid letters
