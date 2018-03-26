428bcf6caba0faf5b9b96528a99bab220f74012f
khemeioa <tasha.s.chin@gmail.com>
2018-03-26 11:15:49 -0700

    Initial thoughts on spoonerize challenge.

---
+++ spoonerize.py
@@ -0,0 +1,10 @@
+def spoonerize(words):
+    # your code here
+    words = words.split()
+    
+    for word in words:
+        word[0]
+    
+    return ("").join(words)
+
+spoonerize("spoonerize me")  # mpoonerize se
