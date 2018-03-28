98b60bcd29710361ef87c0220ff48d38e4e51933
tashachin <tasha.s.chin@gmail.com>
2018-03-28 10:39:21 -0700

    Completed spoonerize me challenge.

--- spoonerize.py
+++ spoonerize.py
@@ -1,10 +1,18 @@
 def spoonerize(words):
-    # your code here
+    # i'm turning the input into a list of strs
+    # i want to swap the strs at 0 with one another but strs are immutable
+    spooned_words = []
     words = words.split()
-    
-    for word in words:
-        word[0]
-    
-    return ("").join(words)
 
-spoonerize("spoonerize me")  # mpoonerize se
+    first_word = words[0]
+    second_word = words[1]
+
+    spooned_words.append(first_word[0] + second_word[1:])
+    spooned_words.append(second_word[0] + first_word[1:])
+    
+    return (" ").join(spooned_words)
+
+print spoonerize("spoonerize me")  # mpoonerize se
+
+# time complexity: O(n) because of .split() and .join()
+# bit more space complexity because slicing creates new strings
